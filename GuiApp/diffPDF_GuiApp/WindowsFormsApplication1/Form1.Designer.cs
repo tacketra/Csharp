02b19be9bc138a057391d79328a57be54a9d8d98
unknown <rtackett@AH-RTACKETT.assurerx.local>
2014-10-21 10:31:34 -0400

    adding gui app version of diffpdfc comparison utility

---
+++ GuiApp/diffPDF_GuiApp/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DiffPdfDirectoryApplicationGUI
+{
+    partial class DiffPdfDirGui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Directory1Button = new System.Windows.Forms.Button();
+            this.Directory2Button = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.ComparisonOutputDirectoryButton = new System.Windows.Forms.Button();
+            this.Directory1Text = new System.Windows.Forms.TextBox();
+            this.Directory2Text = new System.Windows.Forms.TextBox();
+            this.ComparisonOutputDirectoryText = new System.Windows.Forms.TextBox();
+            this.StartComparison = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ComparisonCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Directory1Button
+            // 
+            this.Directory1Button.Location = new System.Drawing.Point(13, 28);
+            this.Directory1Button.Name = "Directory1Button";
+            this.Directory1Button.Size = new System.Drawing.Size(75, 23);
+            this.Directory1Button.TabIndex = 1;
+            this.Directory1Button.Text = "Directory 1";
+            this.Directory1Button.UseVisualStyleBackColor = true;
+            this.Directory1Button.Click += new System.EventHandler(this.Directory1Button_Click);
+            // 
+            // Directory2Button
+            // 
+            this.Directory2Button.Location = new System.Drawing.Point(13, 70);
+            this.Directory2Button.Name = "Directory2Button";
+            this.Directory2Button.Size = new System.Drawing.Size(75, 23);
+            this.Directory2Button.TabIndex = 2;
+            this.Directory2Button.Text = "Directory 2";
+            this.Directory2Button.UseVisualStyleBackColor = true;
+            this.Directory2Button.Click += new System.EventHandler(this.Directory2Button_Click);
+            // 
+            // ComparisonOutputDirectoryButton
+            // 
+            this.ComparisonOutputDirectoryButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.ComparisonOutputDirectoryButton.AutoSize = true;
+            this.ComparisonOutputDirectoryButton.Location = new System.Drawing.Point(12, 114);
+            this.ComparisonOutputDirectoryButton.Name = "ComparisonOutputDirectoryButton";
+            this.ComparisonOutputDirectoryButton.Size = new System.Drawing.Size(152, 23);
+            this.ComparisonOutputDirectoryButton.TabIndex = 3;
+            this.ComparisonOutputDirectoryButton.Text = "Comparison Output Directory";
+            this.ComparisonOutputDirectoryButton.UseVisualStyleBackColor = true;
+            this.ComparisonOutputDirectoryButton.Click += new System.EventHandler(this.ComparisonOutputDirectoryButton_Click);
+            // 
+            // Directory1Text
+            // 
+            this.Directory1Text.Location = new System.Drawing.Point(94, 28);
+            this.Directory1Text.Name = "Directory1Text";
+            this.Directory1Text.Size = new System.Drawing.Size(743, 20);
+            this.Directory1Text.TabIndex = 4;
+            this.Directory1Text.TextChanged += new System.EventHandler(this.Directory1Text_TextChanged);
+            // 
+            // Directory2Text
+            // 
+            this.Directory2Text.Location = new System.Drawing.Point(94, 73);
+            this.Directory2Text.Name = "Directory2Text";
+            this.Directory2Text.Size = new System.Drawing.Size(743, 20);
+            this.Directory2Text.TabIndex = 5;
+            this.Directory2Text.TextChanged += new System.EventHandler(this.Directory2Text_TextChanged);
+            // 
+            // ComparisonOutputDirectoryText
+            // 
+            this.ComparisonOutputDirectoryText.Location = new System.Drawing.Point(170, 117);
+            this.ComparisonOutputDirectoryText.Name = "ComparisonOutputDirectoryText";
+            this.ComparisonOutputDirectoryText.Size = new System.Drawing.Size(667, 20);
+            this.ComparisonOutputDirectoryText.TabIndex = 6;
+            this.ComparisonOutputDirectoryText.TextChanged += new System.EventHandler(this.ComparisonOutputDirectoryText_TextChanged);
+            // 
+            // StartComparison
+            // 
+            this.StartComparison.Location = new System.Drawing.Point(330, 170);
+            this.StartComparison.Name = "StartComparison";
+            this.StartComparison.Size = new System.Drawing.Size(109, 23);
+            this.StartComparison.TabIndex = 7;
+            this.StartComparison.Text = "Start Comparison";
+            this.StartComparison.UseVisualStyleBackColor = true;
+            this.StartComparison.Click += new System.EventHandler(this.StartComparison_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(128, 199);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(541, 23);
+            this.progressBar1.TabIndex = 8;
+            this.progressBar1.Visible = false;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // ComparisonCount
+            // 
+            this.ComparisonCount.BackColor = System.Drawing.SystemColors.Control;
+            this.ComparisonCount.Location = new System.Drawing.Point(676, 201);
+            this.ComparisonCount.Name = "ComparisonCount";
+            this.ComparisonCount.ReadOnly = true;
+            this.ComparisonCount.Size = new System.Drawing.Size(69, 20);
+            this.ComparisonCount.TabIndex = 9;
+            this.ComparisonCount.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 262);
+            this.Controls.Add(this.ComparisonCount);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.StartComparison);
+            this.Controls.Add(this.ComparisonOutputDirectoryText);
+            this.Controls.Add(this.Directory2Text);
+            this.Controls.Add(this.Directory1Text);
+            this.Controls.Add(this.ComparisonOutputDirectoryButton);
+            this.Controls.Add(this.Directory2Button);
+            this.Controls.Add(this.Directory1Button);
+            this.Name = "Form1";
+            this.Text = "DiffPDF Directory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Directory1Button;
+        private System.Windows.Forms.Button Directory2Button;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button ComparisonOutputDirectoryButton;
+        private System.Windows.Forms.TextBox Directory1Text;
+        private System.Windows.Forms.TextBox Directory2Text;
+        private System.Windows.Forms.TextBox ComparisonOutputDirectoryText;
+        private System.Windows.Forms.Button StartComparison;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox ComparisonCount;
+
+
+    }
+}
+
---
+++ GuiApp/diffPDF_GuiApp/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace DiffPdfDirectoryApplicationGUI
+{
+    public partial class DiffPdfDirGui : Form
+    {
+        String reportDir = @"";//path to  direcory where the comparison of the all the pdfs in pdfDir1 vs pdfDir2 will be stored
+        String pdfDir1 = @"", pdfDir2 = @"";
+        String pdf1Name = "", pdf2Name = "";
+
+        public DiffPdfDirGui()
+        {
+            InitializeComponent();
+        }
+
+        private void Directory1Button_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Directory1Text.Text = folderBrowserDialog1.SelectedPath;
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath,"*",  SearchOption.AllDirectories);
+            }
+        }
+
+
+        private void Directory1Text_TextChanged(object sender, EventArgs e) { }
+
+        private void Directory2Button_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Directory2Text.Text = folderBrowserDialog1.SelectedPath;
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*", SearchOption.AllDirectories);
+            }
+        }
+
+        private void Directory2Text_TextChanged(object sender, EventArgs e){}
+        private void ComparisonOutputDirectoryText_TextChanged(object sender, EventArgs e){}
+
+        //probably could add a check to see if passed in directory exists or not
+        private void StartComparison_Click(object sender, EventArgs e)
+        {
+            if (Directory1Text.Text == "")
+            {
+                MessageBox.Show("you need to give an entry for Directory1");
+                return;
+            }
+            else if (Directory2Text.Text == "")
+            {
+                MessageBox.Show("you need to give an entry for Directory2");
+                return;
+            }
+            else if (ComparisonOutputDirectoryText.Text == "")
+            {
+                MessageBox.Show("you need to give an entry for the Comparison Output Directory");
+                return;
+            }
+            pdfDir1 = @Directory1Text.Text;
+            pdfDir2 = @Directory2Text.Text;
+            reportDir = @ComparisonOutputDirectoryText.Text;
+            reportDir += "\\";//will error out if path to report directory doesn't have a backslash at the end, should 
+                              //add error checking to see if it already ends with a backslash
+            String pdf1 = @"";//pdf1 is a single pdf from the directory from path1, pdf2 is a single pdf from the directory for path2
+            String pdf2 = @"";
+            string[] files = Directory.GetFiles(pdfDir1, "*", SearchOption.AllDirectories); //old pdfs from path1 to compare against new pdfs in files2
+            string[] files2 = Directory.GetFiles(pdfDir2, "*", SearchOption.AllDirectories);
+
+            if (files.Length == files2.Length)
+            {
+                
+                ComparisonCount.Visible = true; // 0/3 of files compared , then 1/3 etc.
+                progressBar1.Visible = true; // green bar that fills up equal to the percentage of files done being compared
+                progressBar1.Maximum = files.Length; //if bar max size = 3 for the 3 files, it incremenets by 1/3 each time etc.
+                progressBar1.Minimum = 0;
+                ComparisonCount.Text = 0.ToString() + "/" + (files.Length).ToString();
+                progressBar1.Value = 0;
+                progressBar1.Step = 1;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    progressBar1.PerformStep();
+                    pdf1Name = (files.GetValue(i)).ToString(); //finish adding pdfname to 
+                    pdf2Name = (files2.GetValue(i)).ToString();
+                    int lastSlash = pdf1Name.LastIndexOf("\\"), lastSlash2 = pdf2Name.LastIndexOf("\\");
+                    //Console.WriteLine("last slash pdf1 location is " + lastSlash);
+                    pdf1Name = pdf1Name.Substring(lastSlash + 1, pdf1Name.Length - lastSlash - 1);
+                    pdf2Name = pdf2Name.Substring(lastSlash2 + 1, pdf2Name.Length - lastSlash2 - 1);
+                    pdf1 = (files.GetValue(i)).ToString();
+                    pdf1 = "\"" + pdf1 + "\"";          // slashes are added to the report, pdf1 and pdf2 arguments to get aroud 
+                    //errors if any of the dirs in these contain a name with a space, if pdf1 was @"C:\dir name" it would cause error w/o slash fix
+                    pdf2 = (files2.GetValue(i)).ToString();
+                    pdf2 = "\"" + pdf2 + "\"";
+
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo(
+                        /*"C:\\Program Files (x86)\\DiffPDFc\\diffpdfc",*/"diffpdfc", 
+                         "-r " +  "\"" + reportDir + "report_" + pdf1Name + "_VS_" + pdf2Name + "\"" + " " + pdf1 + " " + pdf2);
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.StartInfo.CreateNoWindow = false;
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                    ComparisonCount.Text = (i+1).ToString() + "/" + (files.Length).ToString();
+                    //progressBar1.PerformStep();
+                }
+                ComparisonCount.Text = "done";
+            }
+            else
+            {
+                Console.WriteLine("directory 1 has " + files.Length + " pdfs and directory 2 has " + files2.Length + " pdfs, they need to contain the same amount of pdfs!");
+            }
+            // Console.ReadLine();
+            
+        }
+
+        private void ComparisonOutputDirectoryButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                ComparisonOutputDirectoryText.Text = folderBrowserDialog1.SelectedPath;
+                string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*", SearchOption.AllDirectories);
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e){}
+
+
+    }
+}
